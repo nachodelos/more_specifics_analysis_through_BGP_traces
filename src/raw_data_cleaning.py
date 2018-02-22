5ff069857ad7f566828d95cffaac7f5f68330caa
Nacho Gutierrez <nachodelos@gmail.com>
2018-02-22 17:55:07 +0100

    Read RIPE file with subprocess

--- src/raw_data_cleaning.py
+++ src/raw_data_cleaning.py
@@ -4,4 +4,10 @@ Spyder Editor
 
 This is a temporary script file.
 """
+import subprocess
+
+file_path = '/Users/nachogutierrez/Documents/traffic_analysis/data/updates.20171030.2335.gz'
+bggdump_path = '/usr/local/bin/bgpdump'
+
+update_lines  = subprocess.check_output ([ bggdump_path, '-m', file_path]).strip().split('\n')
 
