920c0a249959b46a63af7193af4e455a83b214fa
Nacho Gutierrez <nachodelos@gmail.com>
2018-02-23 18:39:32 +0100

    Refactor saving every message even when there are not every field

--- src/raw_data_cleaning.py
+++ src/raw_data_cleaning.py
@@ -19,25 +19,31 @@ def seek_separator( my_str ):
   
 
 def dump_into_lists( my_str, from_to_indexes, times, types, s_IPs, s_AS, prefixes, AS_PATHs):  
-        
-    for k in range(1, len(from_to_indexes)):
+     
+    n_elements = len(from_to_indexes)
+    for k in range(1, n_elements):
         to_index = from_to_indexes[k] - 1
         from_index = from_to_indexes[k-1]
         # Sería correcto hacer esto???
-        if (len(from_to_indexes) > 6) :        
-            if k == 1:
-                times.append( update_lines[i][from_index:to_index])
-            elif k == 2:
-                types.append( update_lines[i][from_index:to_index])
-            elif k == 3:
-                s_IPs.append( update_lines[i][from_index:to_index])
-            elif k == 4:
-                s_AS.append( update_lines[i][from_index:to_index])
-            elif k == 5:
-                prefixes.append( update_lines[i][from_index:to_index])
-            elif k == 6:     
-                AS_PATH_list = update_lines[i][from_index:to_index].split(' ')
-                AS_PATHs.append( AS_PATH_list)
+#        if (len(from_to_indexes) > 6) :        
+        if k == 1:
+            times.append( update_lines[i][from_index:to_index])
+        elif k == 2:
+            types.append( update_lines[i][from_index:to_index])
+        elif k == 3:
+            s_IPs.append( update_lines[i][from_index:to_index])
+        elif k == 4:
+            s_AS.append( update_lines[i][from_index:to_index])   
+            if n_elements <= 5:
+                prefixes.append( '')
+                AS_PATHs.append( [])
+            elif n_elements <= 6:
+                AS_PATHs.append( [])
+        elif k == 5:
+            prefixes.append( update_lines[i][from_index:to_index])
+        elif k == 6:     
+            AS_PATH_list = update_lines[i][from_index:to_index].split(' ')
+            AS_PATHs.append( AS_PATH_list)
     
 # VARIABLES
 file_path = '/Users/nachogutierrez/Documents/traffic_engineering_analysis/data/updates.20171030.2335.gz'
