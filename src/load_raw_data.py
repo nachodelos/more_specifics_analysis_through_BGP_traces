dc2b89875188689c52728b14f2aed16f322565a8
Nacho Gutierrez <nachodelos@gmail.com>
2018-03-05 17:56:23 +0100

    refactor dump_into_list function

--- src/load_raw_data.py
+++ src/load_raw_data.py
@@ -7,44 +7,27 @@ import subprocess
 import pandas as pd
  
 # FUNCTIONS
-def seek_separator( my_str ):
-       
-    from_to_indexes = []
-    str_len = len(my_str)
-        
-    for j in range(str_len):
-        #Stroring element indexes
-        if my_str[j] == '|':
-            from_to_indexes.append( j+1)
-            
-    return  from_to_indexes     
-      
+def dump_into_lists( update_lines, times, types, s_IPs, s_AS, prefixes, AS_PATHs):  
+     
+    m = update_lines.split('|')
+
+    m_type = m[2]
     
-def dump_into_lists( update_lines, from_to_indexes, times, types, s_IPs, s_AS, prefixes, AS_PATHs):  
-         
-    n_elements = len(from_to_indexes)
-    for k in range(1, n_elements):
-        to_index = from_to_indexes[k] - 1
-        from_index = from_to_indexes[k-1]
-          
-        if k == 1:
-            times.append( update_lines[from_index:to_index])
-        elif k == 2:
-            types.append( update_lines[from_index:to_index])
-        elif k == 3:
-            s_IPs.append( update_lines[from_index:to_index])
-        elif k == 4:
-            s_AS.append( update_lines[from_index:to_index])   
-            if n_elements <= 5:
-                prefixes.append( '')
-                AS_PATHs.append( [])
-            elif n_elements <= 6:
-                AS_PATHs.append( [])
-        elif k == 5:
-            prefixes.append( update_lines[from_index:to_index])
-        elif k == 6:     
-            AS_PATH_list = update_lines[from_index:to_index].split(' ')
-            AS_PATHs.append( AS_PATH_list)
+    times.append ( m[1])
+    types.append ( m[2])
+    s_IPs.append ( m[3])
+    s_AS.append ( m[4])
+    
+    if m_type == 'A':
+        prefixes.append( m[5])
+        AS_PATH_list = m[6].split(' ')
+        AS_PATHs.append( AS_PATH_list)        
+    elif m_type == 'W':
+        prefixes.append( m[5])
+        AS_PATHs.append( [])     
+    elif m_type == 'STATE':
+        prefixes.append( '')
+        AS_PATHs.append( [])     
 
 
 # VARIABLES (pathlib)
@@ -64,12 +47,11 @@ to_min = int( to_date.split('.')[1][2:4])
 update_lines = []
 
 for ft in range( from_min, to_min + 1, hop_size):
-    print (ft)
     if(ft<10):
         ft_str = '0'+str(ft)
     else:
         ft_str = str(ft)   
-    
+    print (file_path + ft_str + '.gz')
     update_lines  += subprocess.check_output ([ bggdump_path, '-m', file_path + ft_str + '.gz']).strip().split('\n')
   
 # dump files into list of Strings
@@ -82,16 +64,15 @@ s_IPs = []
 s_AS = []
 prefixes = []
 AS_PATHs = []
-    
+
 # dump data into several lists
 for i in range(len(update_lines)):       
-    from_to_indexes = seek_separator(update_lines[i])
-    dump_into_lists(update_lines[i], from_to_indexes, times, types, s_IPs, s_AS, prefixes, AS_PATHs)       
+    dump_into_lists(update_lines[i],  times, types, s_IPs, s_AS, prefixes, AS_PATHs)       
     
 print (' Data saved as lists!')
 df_update = pd.DataFrame({ 'TIME' : times, 'TYPE': types, 'Source_IP': s_IPs, 'Source_AS': s_AS,'PREFIX': prefixes, 'AS_PATH': AS_PATHs})
 print (' Data Frame created!')
-writer = pd.ExcelWriter(output_file_path + '.gz_2_my_df.xlsx', engine = 'xlsxwriter')
+writer = pd.ExcelWriter(output_file_path + from_date + '-'+ to_date +'.xlsx', engine = 'xlsxwriter')
 df_update.to_excel(writer, sheet_name = 'Sheet1', index_label = 'ID')
 writer.save()
 print(' Excel File saved!')
