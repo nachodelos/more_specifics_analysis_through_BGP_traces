7784b5bebf56981beb82f451659b0991e9605606
Nacho Gutierrez <nachodelos@gmail.com>
2018-03-06 20:26:50 +0100

    Create functions to get affected message indexes

--- src/clean_data.py
+++ src/clean_data.py
@@ -10,11 +10,39 @@ import pandas as pd
 # FUNCTIONS
 def get_STATE_indexes( types):
 	indexes = []
-	for i, m_type in enumerate(types):
-		if (m_type == 'STATE'):
+	for i, m_type in enumerate( types):
+		if ( m_type == 'STATE'):
 			indexes.append( i)
 	return indexes		
 
+def create_window( state_index, times):
+    
+    central_time = int(times[state_index])
+    initial_time = central_time - 5 
+    final_time = central_time + 5
+    
+    if( final_time > int(times[len(times)-1])) :
+        final_time = int(times[len(times)-1])
+        
+    if( initial_time < int(times[0])) :
+        initial_time = int(times[0])
+        
+    return initial_time, final_time
+
+# CLEANING PER MONITOR!!!
+def get_affected_message_indexes( state_index, from_window, to_window, times, monitors):
+    
+    monitor = monitors[state_index]
+    window = range(times.index(from_window), times.index(to_window))
+    
+    affected_indexes = []
+    
+    for i in window:
+        if ( monitor == monitors[i]):
+            affected_indexes.append( i)
+    
+    return affected_indexes
+    
 # VARIABLES (experiment)
 from_date ='20180105.0000' 
 to_date = '20180105.0010'
@@ -35,16 +63,14 @@ df_time_list = df_time_mm.tolist()
 df_type = df['TYPE']
 df_type_list = df_type.tolist()
 
-print(df_time_mm.head() )
-print(df_time_mm.tail() )
+print( df_time_mm.head())
+print( df_time_mm.tail())
 
-state_indexes = get_STATE_indexes(df_type_list)
+state_indexes = get_STATE_indexes( df_type_list)
 print ( len(state_indexes)) 
 
+df_monitor = df['MONITOR']
 
-
-
-
-
-
-
+for i in reversed([605]):
+    from_window, to_window = create_window( i, df_time_list)
+    affected_messages = get_affected_messages_indexes( i, from_window, to_window, df_time_list, df_monitor)
