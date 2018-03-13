0bf25783d7d1c9bd33e1923bc3970a2f423b5cb4
Nacho Gutierrez <nachodelos@gmail.com>
2018-03-13 18:52:27 +0100

    Read and write Excel file

--- src/cluster_updates_into_events.py
+++ src/cluster_updates_into_events.py
@@ -7,4 +7,20 @@ This scripts clusters updates into events acording to several recomendations of 
 """
 import pandas as pd
 
+# VARIABLES (experiment)
+from_date ='20180108.0400' 
+to_date = '20180108.0410'
+input_file_path = '/srv/agarcia/igutierrez/results/rrc00/raw_2_sort_data_for_clustering_updates.' + from_date + '-'+ to_date +'.xlsx'
+output_file_path = '/srv/agarcia/igutierrez/results/rrc00/sort_data_for_clustering_updates_2_events.'
 
+print ( 'Loading ' + input_file_path + '...')
+
+df = pd.read_excel( input_file_path)
+
+df_cluster = df;
+
+writer = pd.ExcelWriter(output_file_path + from_date + '-'+ to_date +'.xlsx', engine = 'xlsxwriter')
+df_cluster.to_excel(writer, sheet_name = 'Sheet1') 
+writer.save()
+
+print(' Excel File saved!')
