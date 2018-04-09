82cb6b4c9bc835bbff5818fc9cda082f24817fb2
Nacho Gutierrez <nachodelos@gmail.com>
2018-04-09 18:09:09 +0200

    Add hours to dumping loop

--- src/load_raw_data.py
+++ src/load_raw_data.py
@@ -8,7 +8,14 @@ import subprocess
 import pandas as pd
 import experiment_manifest as exp
 
-# FUNCTIONS 
+# FUNCTIONS
+def format_number_to_string( num):
+    if num < 10:
+        num_str = '0' + str(num)
+    else:
+        num_str = str(num)
+    return num_str
+
 def dump_into_lists( update_lines, times, types, s_IPs, s_AS, prefixes, AS_PATHs):  
      
     message = update_lines.split('|')
@@ -48,7 +55,7 @@ if (__name__ == '__main__'):
     ris_type = experiment [ 'RISType']
     
     # VARIABLES (pathlib)
-    file_path = '/srv/agarcia/passive_mrai/bgp_updates/' + collector + '/updates.20180108.00'
+    file_path = '/srv/agarcia/passive_mrai/bgp_updates/' + collector + '/updates.20180108.'
     # bggdump_path = '/srv/alutu/bgpdump/bgpdump'
     bgpdump_path = '/usr/local/bin/bgpdump'
     output_file_path = '/srv/agarcia/igutierrez/results/' + exp_name + '/1.load_data/' + collector + '_' + from_date + '-'+ to_date +'.xlsx'
@@ -56,19 +63,50 @@ if (__name__ == '__main__'):
     if (ris_type == 'rrc'):
         hop_size = 5 
  
-    from_min = int( from_date.split('.')[1][2:4])
-    to_min = int( to_date.split('.')[1][2:4])
+    from_min = int(from_date.split('.')[1][2:4])
+    to_min = int(to_date.split('.')[1][2:4])
+
+    from_hour = int(from_date.split('.')[1][0:2])
+    to_hour = int(to_date.split('.')[1][0:2])
+
+    from_day = int(from_date.split('.')[1][0:2])
+    to_day = int(to_date.split('.')[1][0:2])
+
+    from_month = int(from_date.split('.')[1][0:2])
+    to_month = int(to_date.split('.')[1][0:2])
+
+    print(from_date + ' ' + to_date)
     
     update_lines = []
-    
-    for ft in range( from_min, to_min + 1, hop_size):
-        if(ft<10):
-            ft_str = '0'+str(ft)
-        else:
-            ft_str = str(ft)   
-    
-        update_lines  += subprocess.check_output ([ bgpdump_path, '-m', file_path + ft_str]).strip().split('\n')
-        
+
+    for hh in range( from_hour, to_hour + 1, 1):
+
+        hh_str = format_number_to_string(hh)
+
+        if from_hour == to_hour:
+
+            for mm in range( from_min, to_min + 1, hop_size):
+                mm_str = format_number_to_string( mm)
+                print(file_path + hh_str + mm_str)
+                update_lines  += subprocess.check_output ([ bgpdump_path, '-m', file_path + hh_str + mm_str]).strip().split('\n')
+
+        elif to_hour > from_hour:
+
+            if hh == from_hour:
+                from_min_aux = from_min
+                to_min_aux = 56
+            elif from_hour < hh < to_hour:
+                from_min_aux = 0
+                to_min_aux = 56
+            elif hh == to_hour:
+                from_min_aux = 0
+                to_min_aux = to_min
+
+            for mm in range(from_min_aux, to_min_aux + 1, 5):
+                mm_str = format_number_to_string(mm)
+                print(file_path + hh_str + mm_str)
+                update_lines += subprocess.check_output( [bgpdump_path, '-m', file_path + hh_str + mm_str]).strip().split('\n')
+
     # DATA FIELDS
     times = []
     types = []
@@ -88,8 +126,3 @@ if (__name__ == '__main__'):
     df_update.to_excel(writer, sheet_name = 'Sheet1', index = False)
     writer.save()
     print(' Excel File saved!')
-    
-        
-        
-                
-                
