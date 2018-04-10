9d739163e5a1f07aa22650f5c3f5845641568a4b
Nacho Gutierrez <nachodelos@gmail.com>
2018-04-10 17:20:47 +0200

    Add days to dumping loop

--- src/load_raw_data.py
+++ src/load_raw_data.py
@@ -7,105 +7,125 @@ This script dumps data into a proper struct to work more efficiently in Python. 
 import subprocess
 import pandas as pd
 import experiment_manifest as exp
+from calendar import monthrange
+
 
 # FUNCTIONS
-def format_number_to_string( num):
+def format_number_to_string(num):
     if num < 10:
         num_str = '0' + str(num)
     else:
         num_str = str(num)
     return num_str
 
-def dump_into_lists( update_lines, times, types, s_IPs, s_AS, prefixes, AS_PATHs):  
-     
+
+def dump_into_lists(update_lines, times, types, s_IPs, s_AS, prefixes, AS_PATHs):
     message = update_lines.split('|')
-    
+
     m_type = message[2]
-    
-    times.append ( message[1])
-    types.append ( m_type)
-    s_IPs.append ( message[3])
-    s_AS.append ( message[4])
-    
+
+    times.append(message[1])
+    types.append(m_type)
+    s_IPs.append(message[3])
+    s_AS.append(message[4])
+
     if m_type == 'A':
-        prefixes.append( message[5])
+        prefixes.append(message[5])
         AS_PATH_list = message[6].split(' ')
-        AS_PATHs.append( AS_PATH_list)        
+        AS_PATHs.append(AS_PATH_list)
     elif m_type == 'W':
-        prefixes.append( message[5])
-        AS_PATHs.append( [])     
+        prefixes.append(message[5])
+        AS_PATHs.append([])
     elif m_type == 'STATE':
-        prefixes.append( '')
-        AS_PATHs.append( []) 
-           
-        
+        prefixes.append('')
+        AS_PATHs.append([])
+
+
 if (__name__ == '__main__'):
-    
-    print( "---------------")
-    print( "Stage 1: Load Raw Data")
-    print( "---------------")
-    
+
+    print("---------------")
+    print("Stage 1: Load Raw Data")
+    print("---------------")
+
     exp_name, collector = exp.load_arguments()
-        
+
     experiments = getattr(exp, 'experiments')
     experiment = experiments[exp_name]
-    
-    from_date = experiment [ 'initDay']
-    to_date = experiment [ 'endDay']
-    ris_type = experiment [ 'RISType']
-    
+
+    from_date = experiment['initDay']
+    to_date = experiment['endDay']
+    ris_type = experiment['RISType']
+
     # VARIABLES (pathlib)
-    file_path = '/srv/agarcia/passive_mrai/bgp_updates/' + collector + '/updates.20180108.'
-    # bggdump_path = '/srv/alutu/bgpdump/bgpdump'
+    file_path = '/srv/agarcia/passive_mrai/bgp_updates/' + collector + '/'
+    # bgpdump_path = '/srv/alutu/bgpdump/bgpdump'
     bgpdump_path = '/usr/local/bin/bgpdump'
-    output_file_path = '/srv/agarcia/igutierrez/results/' + exp_name + '/1.load_data/' + collector + '_' + from_date + '-'+ to_date +'.xlsx'
+    output_file_path = '/srv/agarcia/igutierrez/results/' + exp_name + '/1.load_data/' + collector + '_' + from_date + '-' + to_date + '.xlsx'
 
     if (ris_type == 'rrc'):
-        hop_size = 5 
- 
+        hop_size = 5
+
     from_min = int(from_date.split('.')[1][2:4])
     to_min = int(to_date.split('.')[1][2:4])
 
     from_hour = int(from_date.split('.')[1][0:2])
     to_hour = int(to_date.split('.')[1][0:2])
 
-    from_day = int(from_date.split('.')[1][0:2])
-    to_day = int(to_date.split('.')[1][0:2])
+    from_day = int(from_date.split('.')[0][6:8])
+    to_day = int(to_date.split('.')[0][6:8])
 
-    from_month = int(from_date.split('.')[1][0:2])
-    to_month = int(to_date.split('.')[1][0:2])
+    from_month = int(from_date.split('.')[0][4:6])
+    to_month = int(to_date.split('.')[0][4:6])
+
+    from_year = int(from_date.split('.')[0][0:4])
+    to_year = int(to_date.split('.')[0][0:4])
 
     print(from_date + ' ' + to_date)
-    
+
     update_lines = []
 
-    for hh in range( from_hour, to_hour + 1, 1):
+    for dd in range(from_day, to_day + 1, 1):
 
-        hh_str = format_number_to_string(hh)
+        dd_str = format_number_to_string(dd)
 
-        if from_hour == to_hour:
+        if from_day == to_day:
+            from_hour_aux = from_hour
+            to_hour_aux = to_hour
+        elif to_day > from_day:
+            if dd == from_day:
+                from_hour_aux = from_hour
+                to_hour_aux = 23
+            elif from_day < dd < to_day:
+                from_hour_aux = 0
+                to_hour_aux = 0
+            elif dd == to_day:
+                from_hour_aux = 0
+                to_hour_aux = to_hour
 
-            for mm in range( from_min, to_min + 1, hop_size):
-                mm_str = format_number_to_string( mm)
-                print(file_path + hh_str + mm_str)
-                update_lines  += subprocess.check_output ([ bgpdump_path, '-m', file_path + hh_str + mm_str]).strip().split('\n')
+        for hh in range(from_hour_aux, to_hour_aux + 1, 1):
 
-        elif to_hour > from_hour:
+            hh_str = format_number_to_string(hh)
 
-            if hh == from_hour:
+            if from_hour == to_hour:
                 from_min_aux = from_min
-                to_min_aux = 56
-            elif from_hour < hh < to_hour:
-                from_min_aux = 0
-                to_min_aux = 56
-            elif hh == to_hour:
-                from_min_aux = 0
                 to_min_aux = to_min
+            else:
+                if hh == from_hour and dd == from_day:
+                    from_min_aux = from_min
+                    to_min_aux = 56
+                elif hh == to_hour and dd == to_day:
+                    from_min_aux = 0
+                    to_min_aux = to_min
+                else:
+                    from_min_aux = 0
+                    to_min_aux = 56
 
             for mm in range(from_min_aux, to_min_aux + 1, 5):
                 mm_str = format_number_to_string(mm)
-                print(file_path + hh_str + mm_str)
-                update_lines += subprocess.check_output( [bgpdump_path, '-m', file_path + hh_str + mm_str]).strip().split('\n')
+                print(file_path + 'updates.201801' + dd_str + '.' + hh_str + mm_str)
+                update_lines += subprocess.check_output(
+                    [bgpdump_path, '-m', file_path + 'updates.201801' + dd_str + '.' + hh_str + mm_str]).strip().split(
+                    '\n')
 
     # DATA FIELDS
     times = []
@@ -114,15 +134,16 @@ if (__name__ == '__main__'):
     s_AS = []
     prefixes = []
     AS_PATHs = []
-        
+
     # dump data into several lists
-    for i in range(len(update_lines)):       
-        dump_into_lists(update_lines[i], times, types, s_IPs, s_AS, prefixes, AS_PATHs)       
-        
+    for i in range(len(update_lines)):
+        dump_into_lists(update_lines[i], times, types, s_IPs, s_AS, prefixes, AS_PATHs)
+
     print (' Data saved as lists!')
-    df_update = pd.DataFrame({ 'TIME' : times, 'TYPE': types, 'MONITOR': s_IPs, 'AS': s_AS,'PREFIX': prefixes, 'AS_PATH': AS_PATHs})
+    df_update = pd.DataFrame(
+        {'TIME': times, 'TYPE': types, 'MONITOR': s_IPs, 'AS': s_AS, 'PREFIX': prefixes, 'AS_PATH': AS_PATHs})
     print (' Data Frame created!')
-    writer = pd.ExcelWriter(output_file_path, engine = 'xlsxwriter')
-    df_update.to_excel(writer, sheet_name = 'Sheet1', index = False)
+    writer = pd.ExcelWriter(output_file_path, engine='xlsxwriter')
+    df_update.to_excel(writer, sheet_name='Sheet1', index=False)
     writer.save()
     print(' Excel File saved!')
