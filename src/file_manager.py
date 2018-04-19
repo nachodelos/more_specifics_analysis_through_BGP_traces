21118746461a48c8a2b510e98b1c0699bdb173fc
Nacho Gutierrez <nachodelos@gmail.com>
2018-04-19 18:26:39 +0200

    Add save_file

---
+++ src/file_manager.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+This script manages input/output files. Basically it is a refactorization of repetitive code in several files
+
+"""
+
+import pandas as pd
+
+
+def save_file(df, ext, output_file_path):
+    if ext == '.xlsx':
+        writer = pd.ExcelWriter(output_file_path, engine='xlsxwriter')
+        df.to_excel(writer, sheet_name='Sheet1', index=False)
+        writer.save()
+        print(' Excel File saved!')
+
+    if ext == '.csv':
+        df.to_csv(output_file_path)
+        print(' CSV File saved!')
