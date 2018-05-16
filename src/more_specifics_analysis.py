e9274d475e6ccbdd8cb4d2d5ff2270f631c32ed1
Nacho Gutierrez <nachodelos@gmail.com>
2018-05-16 17:12:40 +0200

    Add functions to count more specifics

--- src/more_specifics_analysis.py
+++ src/more_specifics_analysis.py
@@ -11,6 +11,7 @@ import file_manager as f
 import pandas as pd
 
 
+# FUNCTIONS
 def get_withdraw_indexes(df):
     df_type = df['TYPE']
 
@@ -19,7 +20,7 @@ def get_withdraw_indexes(df):
     for i in range(len(df)):
         if df_type[i] == 'W':
             updates_withdraw_indexes.append(i)
-    print "Encontrados {} W updates".format(len(updates_withdraw_indexes))
+    print "Find {} W updates".format(len(updates_withdraw_indexes))
 
     return updates_withdraw_indexes
 
@@ -97,6 +98,51 @@ def count_prefixes_per_monitor(dic):
     return monitors, count_pref_per_monitor
 
 
+def group_by_dirIP_mask(dic):
+    reformat_dic = {}
+
+    for monitor in dic:       
+        dir_ip_per_monitor = {} # Each dictionary element has the following format {'monitor_1':{'dir_IP_1':['/20', '/21' ...], 'monitor_2': ...}
+        for pref in dic[monitor]:
+            splitted_pref = pref.split('/')
+            dir_IP = splitted_pref[0]
+            mask = splitted_pref[1]
+            
+            if dir_IP not in dir_ip_per_monitor:
+                dir_ip_per_monitor[dir_IP] = [mask]
+            else:
+                dir_ip_per_monitor[dir_IP].append(mask)
+        
+        reformat_dic[monitor] = dir_ip_per_monitor        
+    
+    return reformat_dic        
+
+
+def count_more_specifics(dic):
+    count_more_specifics_per_monitor = []
+    
+    for monitor in dic:
+        count_more_specifics = 0
+        for dir_IP in dic[monitor]:
+            if len(dic[monitor][dir_IP]) > 1:
+                count_more_specifics = count_more_specifics + (len(dic[monitor][dir_IP]) - 1)
+#            elif len(dic[monitor][dir_IP]) == 1:
+#                count_more_specifics = count_more_specifics + len(dic[monitor][dir_IP])
+                
+        count_more_specifics_per_monitor.append(count_more_specifics)
+        
+    return count_more_specifics_per_monitor
+
+
+def calculate_more_specific_ratio(ms_count, pr_count):
+    more_specific_ratio = []    
+
+    for i in range(len(ms_count)):
+        more_specific_ratio.append(float(ms_count[i])/float(pr_count[i]))
+    
+    return more_specific_ratio    
+    
+    
 if __name__ == "__main__":
 
     print("---------------")
@@ -127,7 +173,7 @@ if __name__ == "__main__":
     input_file_path = result_directory + exp_name + '/3.data_cleaning/' + collector + '_' + from_date + '-' + to_date + file_ext
     output_file_path = result_directory + exp_name + step_dir + '/' + collector + '_' + from_date + '-' + to_date + file_ext
 
-    write_flag = f.overwrite_file(output_file_path)
+    write_flag =  1# f.overwrite_file(output_file_path)
 
     if write_flag == 1:
         print "Loading " + input_file_path + "..."
@@ -151,11 +197,14 @@ if __name__ == "__main__":
 
         # IPv4 Analysis
         pref_IPv4 = get_prefixes_seen_per_monitor(df_IPv4_updates)
-        monitors_IPv4, pref_IPv4_counts = count_prefixes_per_monitor(pref_IPv4)
+        monitors_IPv4, pref_IPv4_count = count_prefixes_per_monitor(pref_IPv4)
+        refactor_dir_IPv4 = group_by_dirIP_mask(pref_IPv4)
+        more_specifics_count_per_monitor = count_more_specifics(refactor_dir_IPv4)
+        more_specific_ratio = calculate_more_specific_ratio(more_specifics_count_per_monitor, pref_IPv4_count)
 
         step_dir = '/4.more_specifics_analysis/IPv4'
         output_file_path = result_directory + exp_name + step_dir + '/' + collector + '_' + from_date + '-' + to_date + file_ext
-        df_results_count = pd.DataFrame({'MONITOR': monitors_IPv4, 'PREF_COUNT': pref_IPv4_counts})
+        df_results_count = pd.DataFrame({'MONITOR': monitors_IPv4, 'PREF_COUNT': pref_IPv4_count, 'MORE_SPECIFICS_COUNT': more_specifics_count_per_monitor, 'MORE_SPECIFIC_RATIO': more_specific_ratio})
         f.save_file(df_results_count, file_ext, output_file_path)
 
         # IPv6 Analysis
