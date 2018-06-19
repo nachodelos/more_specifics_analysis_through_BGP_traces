8441cdc9a979236ca1c533597105adb93406c3b7
Nacho Gutierrez <nachodelos@gmail.com>
2018-06-19 22:55:11 +0200

    Fix get_affected_message_indexes_per_STATE bug and add cases to test

--- src/test_clean_data.py
+++ src/test_clean_data.py
@@ -1,7 +1,25 @@
-import unittest
-import sort_updates_for_cleaning
+import unittest2 as unittest
+import clean_data
+import os
 
-class TestSort(unittest.TestCase):
+class TestCleanData(unittest.TestCase):
 
-	def test_file_length:
-		
+	# CASE 1: When there is only one message affected corresponding to the STATE index
+	def test_affected_indexes_case_1(self):
+		result = clean_data.get_affected_message_indexes_per_STATE( 2, ['1', '1', '1', '1', '1'], ['A', 'A', 'STATE', 'A', 'A'], ['1', '2', '12', '18', '60'])
+		self.assertEqual(result , [2])
+ 
+	# CASE 2: When there is more than a message affected (Including STATE index) but only backward
+	def test_affected_indexes_case_2(self):	
+		result = clean_data.get_affected_message_indexes_per_STATE( 2, ['1', '1', '1', '1', '1'], ['A', 'A', 'STATE', 'W', 'A'], ['1', '2', '3', '9', '60'])
+		self.assertEqual(result , [0, 1, 2])
+
+	# CASE 3: When there is more than a message affected (Including STATE index) but only forward
+	def test_affected_indexes_case_3(self):	
+		result = clean_data.get_affected_message_indexes_per_STATE( 2, ['1', '1', '1', '1', '1'], ['A', 'A', 'STATE', 'W', 'A'], ['1', '2', '12', '14', '17'])
+		self.assertEqual(result , [2, 3, 4])
+ 
+
+
+
+        
