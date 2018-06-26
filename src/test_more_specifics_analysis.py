e839dcd2779c1ed34741155d2b20e0ff33733506
Nacho Gutierrez <nachodelos@gmail.com>
2018-06-26 18:54:37 +0200

    test cluster_advises_per_monitor

--- src/test_more_specifics_analysis.py
+++ src/test_more_specifics_analysis.py
@@ -98,7 +98,7 @@ class TestMoreSpecificsAnalysis(unittest.TestCase):
 
         self.assertEqual(expected_IPv6_A['PREFIX'].all(), result_IPv6_A['PREFIX'].all())
 
-    def test_get_IPv6_prefixes_seen_per_monitor(self):
+    def test_get_IPv4_prefixes_seen_per_monitor(self):
         result = more_specifics_analysis.get_prefixes_seen_per_monitor({'TIME': ['1', '3', '4', '5'],
                                                                         'TYPE': ['A', 'A', 'A', 'A'],
                                                                         'MONITOR': ['193.0.0.55', '193.0.0.56',
@@ -121,7 +121,7 @@ class TestMoreSpecificsAnalysis(unittest.TestCase):
 
         self.assertEqual(result, expected_result)
 
-    def test_get_IPv4_prefixes_seen_per_monitor(self):
+    def test_get_IPv6_prefixes_seen_per_monitor(self):
         result = more_specifics_analysis.get_prefixes_seen_per_monitor({'TIME': ['1', '3', '4', '5'],
                                                                         'TYPE': ['A', 'A', 'A', 'A'],
                                                                         'MONITOR': ['2a01:2a8::3', '193.0.0.56',
@@ -143,12 +143,45 @@ class TestMoreSpecificsAnalysis(unittest.TestCase):
         self.assertEqual(result, expected_result)
 
     def test_count_prefixes_per_monitor(self):
-        monitors, counts_per_monitor = more_specifics_analysis.count_prefixes_per_monitor({'2a01:2a8::3': ['2001:7fb:fe05::/48'],
-                                                                     '193.0.0.56': ['2a06:e881:1400::/47',
-                                                                                    '2001:7fb:fe05::/48']})
+        monitors, counts_per_monitor = more_specifics_analysis.count_prefixes_per_monitor(
+            {'2a01:2a8::3': ['2001:7fb:fe05::/48'],
+             '193.0.0.56': ['2a06:e881:1400::/47',
+                            '2001:7fb:fe05::/48']})
 
         self.assertEqual(monitors, ['193.0.0.56', '2a01:2a8::3'])
         self.assertEqual(counts_per_monitor, [2, 1])
+
+    def test_cluster_advises_per_monitor(self):
+        least_specifics, more_specifics, intermediates, uniques = more_specifics_analysis.cluster_advises_per_monitor(
+            {'193.0.0.55': ['118.184.200.0/31',
+                            '118.184.200.0/32',
+                            '118.184.200.1/32',
+                            '118.184.200.0/30'],
+             '193.0.0.56': ['118.184.200.0/21',
+                            '78.130.255.0/24',
+                            '213.145.111.0/24']})
+        expected_least_specifics = {'193.0.0.55': ['118.184.200.0/30'],
+                                    '193.0.0.56': []}
+
+        self.assertEqual(expected_least_specifics, least_specifics)
+
+        expected_more_specifics = {'193.0.0.55': ['118.184.200.0/32',
+                                                  '118.184.200.1/32'],
+                                   '193.0.0.56': []}
+
+        self.assertEqual(more_specifics, expected_more_specifics)
+
+        expected_intermediates = {'193.0.0.55': ['118.184.200.0/31'],
+                                  '193.0.0.56': []}
+
+        self.assertEqual(intermediates, expected_intermediates)
+
+        expected_uniques = {'193.0.0.55': [],
+                            '193.0.0.56': ['118.184.200.0/21',
+                                           '78.130.255.0/24',
+                                           '213.145.111.0/24']}
+
+        self.assertEqual(expected_uniques, uniques)
 
 
 if __name__ == '__main__':
